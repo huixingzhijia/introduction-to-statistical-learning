56cacfbcfdd4984f786d2cf58c3849ec7f2bf527
huixingzhijia <zengw@slu.edu>
2017-04-22 11:17:25 -0500

    Add files via upload

--- Chapter10/chapter10exercise.R
+++ Chapter10/chapter10exercise.R
@@ -1,3 +1,112 @@
+#2
+
+##a-e
+d = as.dist(matrix(c(0, 0.3, 0.4, 0.7, 
+                     0.3, 0, 0.5, 0.8,
+                     0.4, 0.5, 0.0, 0.45,
+                     0.7, 0.8, 0.45, 0.0), nrow=4))
+plot(hclust(d, method="complete"))
+plot(hclust(d, method="single"))
+plot(hclust(d, method="complete"), labels=c(2,1,4,3))
+
+#If the results in (a) have two cluster. We can see that (1,2) and (3,4)
+#The two clusters are (4), (1,2,3)
+
+##3
+
+##a
+set.seed(1)
+x1<-c(1,1,0,5,6,4)
+x2<-c(4,3,4,1,2,0)
+data<-cbind(x1,x2)
+plot(data[,1],data[,2])
+plot(data)
+
+##b
+s<-sample(2,nrow(data),replace = T)
+plot(data[,1],data[,2],col=s+1,pch=20,cex=2)
+
+##c
+
+centroid1<-c(mean(data[s==1,1]),mean(data[s==1,2]))
+centroid2<-c(mean(data[s==2,1]),mean(data[s==2,2]))
+plot(data[,1],data[,2],col=s+1,pch=20,cex=2)
+points(centroid1[1],centroid1[2],col=1,pch=4)
+points(centroid2[1],centroid2[2],col=2,pch=4)
+
+##d
+euclid = function(x1, x2) {
+  return(sqrt((x1[1] - x2[1])^2 + (x1[2]-x2[2])^2))
+}
+assign_labels = function(x, centroid1, centroid2) {
+  s = rep(NA, nrow(x))
+  for (i in 1:nrow(x)) {
+    if (euclid(x[i,], centroid1) < euclid(x[i,], centroid2)) {
+      s[i] = 1
+    } else {
+      s[i] = 2
+    }
+  }
+  return(s)
+}
+labels = assign_labels(data, centroid1, centroid2)
+labels
+
+##e
+
+centroid1<-c(mean(data[s==1,1]),mean(data[s==1,2]))
+centroid2<-c(mean(data[s==2,1]),mean(data[s==2,2]))
+plot(data[,1],data[,2],col=s+1,pch=20,cex=2)
+points(centroid1[1],centroid1[2],col=1,pch=4)
+points(centroid2[1],centroid2[2],col=2,pch=4)
+
+#In here each observation was assigned to the closest centroid
+
+#f
+plot(data[,1],data[,2],col=s+1,pch=20,cex=2)
+#Point 1,2,3 was in one cluster and point 4,5,6 in another
+
+#5
+
+##a
+
+socks<-c(10,9,8,7,6,5,10)
+computers<-c(0,0,0,1,1,1,1)
+shop<-cbind(socks,computers)
+labels<-c(1,1,1,2,2,2,1)
+plot(shop[,1],shop[,2],col=labels+1,pch=20,cex=2)
+
+##b
+
+socks<-scale(socks,center = F)
+computers<-scale(computers,center = F)
+shop<-cbind(socks,computers)
+labels<-c(1,1,1,2,2,2,1)
+plot(shop[,1],shop[,2],col=labels+1,pch=20,cex=2)
+
+#6
+
+#c
+
+set.seed(10)
+control<-matrix(rnorm(50*1000),ncol = 50)
+treatment<-matrix(rnorm(50*1000),ncol = 50)
+data<-cbind(control,treatment)
+data[1,]<-seq(-20,20-0.4,.4)#Linear trend in one dimension
+
+prc<-prcomp(scale(data))
+summary(prc)$importance[,1]
+#0.11541 explained by the first principle
+
+#Now addint A vs B via 10 vs 0 coding
+
+data<-rbind(data,c(rep(10,50),rep(0,50)))
+prc<-prcomp(data,scale=T)
+summary(prc)$importance[,1]
+#0.147900  of the variables was explained.It was showing that addint the A vs B
+#improving the variation been explained. 
+
+
 #7
 library(ISLR)
 set.seed(2)
